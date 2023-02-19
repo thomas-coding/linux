7adaf5280ac0c5290782157fc363564fff1ac005
Jinping Wu <jinping.wu@verisilicon.com>
2023-02-19 18:48:13 +0800

    a55: add userspace hello world depend on initramfs

---
+++ samples/ramfs/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+
+# Linux 64
+export PATH="/home/cn1396/.toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-linux-gnu/bin/:$PATH"
+
+# For cross compile
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+
+rm -rf image
+
+# make /dev/console for printf
+mkdir -p image/dev
+sudo mknod -m 660 image/dev/console c 5 1 
+
+# create init
+aarch64-none-linux-gnu-gcc -g -static -o image/init hello.c
+
+#echo hello | cpio -o --format=newc > initramfs
---
+++ samples/ramfs/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Userspace Hello World\n");
+    fflush(stdout);
+    fflush(stdout);
+    while(1);
+}
+
