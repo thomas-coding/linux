d039e86c99d31d8b675fc3f7ea6ebeb9ccd9f4f7
Jinping Wu <jinping.wu@verisilicon.com>
2023-05-14 15:36:29 +0800

    a55: refine userspace demo compile

--- samples/userspace_test/build.sh
+++ samples/userspace_test/build.sh
@@ -14,21 +14,22 @@ rm -rf ${current_folder}/out
 mkdir -p ${current_folder}/out
 
 # create binary
-aarch64-none-linux-gnu-gcc -g -static -o out/hello hello/hello.c
+aarch64-none-linux-gnu-gcc -g -static -o out/hello hello/hello.c || exit
 aarch64-none-linux-gnu-gcc -g -o out/ctest crypto/ctest.c \
     crypto/crypto_data.c crypto/crypto_util.c \
-    crypto/crypto_hash_test.c
-aarch64-none-linux-gnu-gcc -g -static -o out/block_device block_device/block_device.c
+    crypto/crypto_hash_test.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/block_device block_device/block_device.c || exit
 
-aarch64-none-linux-gnu-gcc -g -static -o out/udp_server net/udp_server.c
-aarch64-none-linux-gnu-gcc -g -static -o out/udp_client net/udp_client.c
-aarch64-none-linux-gnu-gcc -g -static -o out/tcp_server net/tcp_server.c
-aarch64-none-linux-gnu-gcc -g -static -o out/tcp_client net/tcp_client.c
+aarch64-none-linux-gnu-gcc -g -static -o out/udp_server net/udp_server.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/udp_client net/udp_client.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/tcp_server net/tcp_server.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/tcp_client net/tcp_client.c || exit
 
-aarch64-none-linux-gnu-gcc -g -static -o out/ucontext ucontext/ucontext.c
-aarch64-none-linux-gnu-gcc -g -static -o out/debuggee ptrace/debuggee.c
-aarch64-none-linux-gnu-gcc -g -static -o out/debugger ptrace/debugger.c
+aarch64-none-linux-gnu-gcc -g -static -o out/ucontext ucontext/ucontext.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/debuggee ptrace/debuggee.c || exit
+aarch64-none-linux-gnu-gcc -g -static -o out/debugger ptrace/debugger.c || exit
+
+aarch64-none-linux-gnu-gcc -g -static -o out/display display/display.c || exit
 
 # dump asm
 aarch64-none-linux-gnu-objdump -xd out/debuggee > out/debuggee.asm
-
